3b8e4725e3a8e2d8dbca35f84114fd41c36a4ea4
Ralph Castellanos <rcastellanos168@unab.edu.co>
2025-09-09 13:55:20 -0500

    Rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/products', function () {
+    return "Products list";
+});
+
+Route::get('/products/{id}/{category?}', function ($id, $category = null) {
+    if ($category != null){
+        return "Detail product: " . $id . ". With Category: ". $category;
+    }
+    else{
+        return "Detail product: " . $id;
+    }
 });
