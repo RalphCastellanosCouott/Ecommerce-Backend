dd934b938884f9dd75d45e38e01588d4b6dedbcd
Ralph Castellanos <rcastellanos168@unab.edu.co>
2025-09-30 15:03:55 -0500

    ProductFactory

--- database/migrations/2025_09_16_192544_create_images_product_table.php
+++ database/migrations/2025_09_16_192544_create_images_product_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
             $table->id();
 
             $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('product');
+            $table->foreign('product_id')->references('id')->on('products');
 
             $table->string('url');
             $table->timestamps();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class
         ]);
+
+        Product::factory(1000)->create();
     }
 }
