026d579b0e8776b99bbe012d537b501dd6506643
Ralph Castellanos <rcastellanos168@unab.edu.co>
2025-09-30 14:38:55 -0500

    CategoryFactory

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -17,5 +17,7 @@ class CategorySeeder extends Seeder
         $categoryComputers = new Category();
         $categoryComputers->name = 'Computers';
         $categoryComputers->save();
+
+        Category::factory(200)->create();
     }
 }
