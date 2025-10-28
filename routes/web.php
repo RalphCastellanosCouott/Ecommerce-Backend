dc995eef39a91ba04bce06561c9190114fe11449
luis bohorquez <lbohorquez662@unab.edu.co>
2025-10-28 14:38:52 -0500

    Layouts Admin

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class,'index']);
 
 Route::prefix('products')-> controller(ProductController::class)->group(function () {
     Route::get('/', 'index');
@@ -17,4 +17,8 @@ Route::prefix('products')-> controller(ProductController::class)->group(function
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/', [HomeController::class, 'welcome']);
+
+Route::prefix('admin')->group(function(){
+    Route::get('/', [AdminController::class,'index'])->name('admin.index');
+    Route::get('/categories', [CategoryController::class,'create'])->name('admin.categories.create');
+});
