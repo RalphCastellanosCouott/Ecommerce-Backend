b89271689c8a4233a6c89a5e262e0c90b405be12
Ralph Castellanos <rcastellanos168@unab.edu.co>
2025-11-13 22:36:28 -0500

    Ajuste de los seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run()
+    public function run(): void
     {
         $this->call([
-            CategorySeeder::class
+            CategorySeeder::class,
+            BrandSeeder::class
         ]);
 
         Product::factory(1000)->create();
